bd7a13542ba5e1d66485b14fc9e00983502d2167
pikman34 <noahbob34@gmail.com>
2026-09-20 23:59:40 +0100

    basic ai movement

---
+++ Human Viruses GULP/Assets/Scripts/MoveToMouse.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToMouse : MonoBehaviour
+{
+    public static List<MoveToMouse> moveableObjects = new List<MoveToMouse>();
+    private Vector3 target;
+    private bool selected;
+    private NavMeshAgent agent;
+
+
+    void Start()
+    {
+        moveableObjects.Add(this);
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && selected)
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+            agent.SetDestination(target);
+        }
+        
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1))
+        { 
+            selected = true;
+            gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+
+            foreach(MoveToMouse obj in moveableObjects)
+            {
+                if (obj != this)
+                {
+                    obj.selected = false;
+                    obj.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+                }
+            }
+        }
+
+    }
+}
